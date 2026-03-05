87f6600b23d8de57029c353cf135d7bbe388bb4a
user1 <samaralbouchi03@gmail.com>
2026-03-05 22:53:16 +0100

    fix: allow all origins in CORS config

--- springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
+++ springboot/app/src/main/java/com/oauth/demo/config/CorsConfig.java
@@ -1,17 +1,14 @@
 package com.oauth.demo.config;
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
-
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://192.168.0.183:4200") // Add your allowed origins here
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
 }
